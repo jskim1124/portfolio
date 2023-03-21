8d7dbd29e31161790c233e3c7a30dd5a428e0ea0
jskim1124 <jskim1124@snu.ac.kr>
2023-03-21 12:27:15 +0900

    프로젝트 추가 오류
    버튼을 누르면 표에 뜨게 하고 싶은데 안됨

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-const addProjectButton = document.querySelector('#add-project-button');
-const projectTableBody = document.querySelector('table tbody');
-
-addProjectButton.addEventListener('click', () => {
-  const projectNameInput = document.querySelector('#project-name-input');
-  const dateInput = document.querySelector('#date-input');
-  const descriptionInput = document.querySelector('#description-input');
+function addProject() {
+    const projectName = document.getElementById("name-input").value;
+    const projectDate = document.getElementById("date-input").value;
+    const projectDescription = document.getElementById("description-input").value;
+    
+    // 새로운 <tr> 요소를 생성하여 <tbody>에 추가
+    const tbody = document.querySelector("table tbody");
+    const newRow = tbody.insertRow();
+    const nameCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const descriptionCell = newRow.insertCell();
+    nameCell.textContent = projectName;
+    dateCell.textContent = projectDate;
+    descriptionCell.textContent = projectDescription;
+    
+    // 입력 폼 초기화
+    document.getElementById("name-input").value = "";
+    document.getElementById("date-input").value = "";
+    document.getElementById("description-input").value = "";
+  }
   
-  const newRow = document.createElement('tr');
-  newRow.innerHTML = `
-    <td>${projectNameInput.value}</td>
-    <td>${dateInput.value}</td>
-    <td>${descriptionInput.value}</td>
-  `;
+  // Add Project 버튼 요소 가져오기
+  const addProjectButton = document.getElementById("add-project-button");
   
-  projectTableBody.appendChild(newRow);
-});
+  // Add Project 버튼 클릭 이벤트 추가
+  addProjectButton.addEventListener("click", function(event) {
+    event.preventDefault(); // 기본 동작 취소
+    addProject();
+  });
